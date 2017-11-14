585354dcfd06ecad956cdd534fcc5fe5ffec8d2e
davidemar <davidemar@users.noreply.github.com>
2017-11-14 12:39:27 -0600

    Initial Commit

---
+++ SoundPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundPlayer
+//
+//  Created by David Mar on 11/14/17.
+//  Copyright © 2017 Piso13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
